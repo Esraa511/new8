c0cd8b313d82d0a2659f40a92175313754baf57a
Esraa511 <136280275+Esraa511@users.noreply.github.com>
2023-06-13 00:56:50 +0300

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,14 @@
+var left=document.querySelector("#nav");
+var x=document.querySelector("#long")
+var right =document.querySelector(".main");
+var btn=document.getElementById("btn");
+var end=document.querySelector("#end")
 
+btn.onclick=function(){
+    x.style.visibility="visible"
+    x.style.width="80vw"
+    x.style.height="80vh"
+}
+end.onclick=function(){
+    x.style.visibility="hidden"
+}
